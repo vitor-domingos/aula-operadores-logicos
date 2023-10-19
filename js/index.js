8f2c2693b22f6bf198f392b4aa5574e75a4a45cf
Vitor Domingos <vm486092@gmail.com>
2023-10-18 23:06:18 -0200

    adicionada a pasta js

---
+++ js/index.js
@@ -0,0 +1,35 @@
+const a = 3 
+const b = "3"
+console.log(a == b) // Verifica se a é igual a b (valor), imprime o resultado
+
+const c = "3"
+const d = "3"
+console.log(c === d) // Verifica se c é estritamente igual a d (valor e tipo), imprime o resultado
+
+const e = "5"
+const f =  3 
+console.log(e !== f) // Verifica se e é diferente de f (valor), imprime o resultado
+
+const i = "7"
+const j =  7
+console.log(i != j) // Verifica se i é diferente de j (valor), imprime o resultado
+
+const m = 10
+const n = 5
+console.log(m < n) // Verifica se m é menor que n, imprime o resultado
+
+const o = 10
+const p = 10 
+console.log(o < p) // Verifica se o é menor que p, imprime o resultado
+
+const q = 15
+const r = 20
+console.log(q < r) // Verifica se q é menor que r, imprime o resultado
+
+const s = 15
+const t = 20
+console.log(s > t) // Verifica se s é maior que t, imprime o resultado
+
+const u = 25
+const v = 25
+console.log( u <= v) // Verifica se u é menor ou igual a v, imprime o resultado
